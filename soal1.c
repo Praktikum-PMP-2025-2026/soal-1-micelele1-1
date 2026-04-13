8b6515e9ca70748f03bfb2b93f7e3e273777cf76
micelele1 <michelleyeo36@gmail.com>
2026-04-13 08:53:15 +0700

    Fix formatting and improve readability in soal1.c

--- soal1.c
+++ soal1.c
@@ -12,7 +12,7 @@
 
 int status(int izin, int suhu, int radiasi, int jam ){
     if(radiasi>=6){ printf("TOLAK\n");}
-    else if (suhu>390) {printf("KARANTINA\n");}
+    else if (suhu>=390) {printf("KARANTINA\n");}
     else if (izin==1&&(jam<6||jam>20)) { printf("TOLAK\n");}
     else if (izin==1){ printf("MASUK\n");}
     else if (izin==2 && radiasi<=2 && (jam>=8&&jam<=18)) {printf("MASUK\n");}
