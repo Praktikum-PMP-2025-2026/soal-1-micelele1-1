76b455b416820da33e221072896042549816c681
micelele1 <michelleyeo36@gmail.com>
2026-04-13 08:57:54 +0700

    Fix formatting and remove unnecessary characters

--- soal1.c
+++ soal1.c
@@ -11,14 +11,14 @@
 #include<stdio.h>
 
 int status(int izin, int suhu, int radiasi, int jam ){
-    if(radiasi>=6){ printf("TOLAK\n");}
-    else if (suhu>=390) {printf("KARANTINA\n");}
-    else if (izin==1&&(jam<6||jam>20)) { printf("TOLAK\n");}
-    else if (izin==1){ printf("MASUK\n");}
-    else if (izin==2 && radiasi<=2 && (jam>=8&&jam<=18)) {printf("MASUK\n");}
+    if(radiasi>=6){ printf("TOLAK");}
+    else if (suhu>=390) {printf("KARANTINA");}
+    else if (izin==1&&(jam<6||jam>20)) { printf("TOLAK");}
+    else if (izin==1){ printf("MASUK");}
+    else if (izin==2 && radiasi<=2 && (jam>=8&&jam<=18)) {printf("MASUK");}
     else if (izin==2){ printf("PEMERIKSAAN\n");}
-    else if(izin==3&&radiasi==0&&(jam>=8&&jam<=18)) {printf ("MASUK\n");}
-    else { ("TOLAK\n");}
+    else if(izin==3&&radiasi==0&&(jam>=8&&jam<=18)) {printf ("MASUK");}
+    else { ("TOLAK");}
 }
 int main (){
     int izin, radiasi, jam;
